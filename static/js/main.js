3b6c5a19284f6ac154ac06450462e29538068ef4
RishiKr3101 <rishikrofficial@gmail.com>
2021-05-05 19:06:26 +0530

    animate navbar responsive

--- static/js/main.js
+++ static/js/main.js
@@ -1,6 +1,13 @@
 const toggleButton = document.getElementsByClassName('toggle-button')[0]
 const navbar = document.getElementsByClassName('navbar-links')[0]
+const circle = document.getElementsByClassName('circle')[0]
+
+const collapse_link = document.getElementsByClassName('collapse-link')[0]
 
 toggleButton.addEventListener('click', () => {
-    navbar.classList.toggle('active')
+    collapse_link.classList.toggle('active')
+})
+
+toggleButton.addEventListener('click', () => {
+    circle.classList.toggle('active')
 })
