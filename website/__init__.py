c98be4e4e6aa76ffebad15cc7c7119df0c826db8
RishiKr3101 <rishikrofficial@gmail.com>
2021-05-07 22:19:00 +0530

    Update

--- website/__init__.py
+++ website/__init__.py
@@ -1,15 +1,22 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+
+db=SQLAlchemy()
+DB_NAME = "database.db"
 
 def create_app():
 
     app = Flask(__name__)
     app.config['SECRET_KEY'] = '1013ramukihsir'
+    app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
+    db.init_app(app)
 
     from .views import views
     from .auth import auth
 
 
-    app.register_blueprint(views, url_prefix='/home')
+    app.register_blueprint(views, url_prefix='/')
     app.register_blueprint(auth, url_prefix='/')
 
 
--- website/auth.py
+++ website/auth.py
@@ -1,9 +1,39 @@
-from flask import Blueprint
+from flask import Blueprint, render_template, request, flash
 
 
 auth = Blueprint('auth', __name__)
 
 
-@auth.route('/')
+@auth.route('/',methods=["POST", "GET"])
 def login():
-    return "<h1> WTF </h1>"
+    return render_template("login.html")
+
+
+@auth.route('/signup', methods=["POST", "GET"])
+def signup():
+    if request.method == 'POST':
+        email = request.form.get('email')
+        firstName = request.form.get('firstName')
+        password1 = request.form.get('password1')
+        password2 = request.form.get('password2')
+
+        if len(email)< 4:
+            flash('Email error', category='error')
+        elif len(firstName)< 2:
+            flash('Name error', category='error')
+        elif password1 != password2:
+            flash ('password not matching', category='error')
+        elif len(password1) <7:
+            flash('password short', category='error')
+        else:
+            flash('Account created', category='success')
+
+
+
+
+    return render_template("signup.html")
+
+
+@auth.route('/logout')
+def logout():
+    return "<h1>logout</h1>"
