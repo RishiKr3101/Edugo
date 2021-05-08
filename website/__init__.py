00c7e1e954d97d049eae134bb255dad0f79eaa23
RishiKr3101 <rishikrofficial@gmail.com>
2021-05-08 20:46:27 +0530

    Authentication

--- website/__init__.py
+++ website/__init__.py
@@ -1,7 +1,7 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from os import path
-
+from flask_login import LoginManager
 
 db=SQLAlchemy()
 DB_NAME = "database.db"
@@ -13,6 +13,8 @@ def create_app():
     app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
     db.init_app(app)
 
+    
+
     from .views import views
     from .auth import auth
 
@@ -23,6 +25,15 @@ def create_app():
     from .models import User, Note
 
     create_database(app)
+
+    login_manager = LoginManager()
+    login_manager.login_view = 'auth.login'
+    login_manager.init_app(app)
+
+
+    @login_manager.user_loader
+    def load_user(id):
+        return User.query.get(int(id))
 
     return app
 
--- website/static/js/main.js
+++ website/static/js/main.js
@@ -1,6 +1,9 @@
 const toggleButton = document.getElementsByClassName('toggle-button')[0]
-const navbar = document.getElementsByClassName('navbar-links')[0]
+
 const circle = document.getElementsByClassName('circle')[0]
+
+const main_content = document.getElementsByClassName('main-content')[0]
+
 
 const collapse_link = document.getElementsByClassName('collapse-link')[0]
 
@@ -10,4 +13,8 @@ toggleButton.addEventListener('click', () => {
 
 toggleButton.addEventListener('click', () => {
     circle.classList.toggle('active')
+})
+
+toggleButton.addEventListener('click', () => {
+    main_content.classList.toggle('active')
 })
